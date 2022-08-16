622882b97c8545c992cbad3c290611fb360c775a
Parv Sharma <pscoder10462@gmail.com>
2022-08-16 16:48:23 +0530

    _redirect for netlify

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseposts: process.env.REACT_APP_SERVER_CONNECTION_URL });
+const API = axios.create({ baseURL: process.env.REACT_APP_SERVER_CONNECTION_URL });
 
 API.interceptors.request.use((req)=> {
 	if (localStorage.getItem('profile'))
